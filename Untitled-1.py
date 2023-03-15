e432b678cc5eb07542f902d8d2143ffaff833644
gabinhio91 <114810717+gabinhio91@users.noreply.github.com>
2023-03-15 14:54:23 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,94 @@
+# def printare():
+#     print("abra")
+#     print("ca")
+#     print("dabra")
+
+# printare()
+
+# def suma(a,b):
+#     suma_2_numere = a+b
+#     return suma_2_numere
+#     print(suma_2_numere)
+
+# suma(2,3)
+
+# alta_suma = 3+ suma(5,5)
+# print(alta_suma)
+
+
+# print(pow(2,3))
+# print(2**3)
+# import math 
+# print(math.sqrt(64))
+
+
+# def ma(a,b,c):
+#     ma = (a+b+c)/3
+#     print(ma)
+#     return ma 
+
+
+
+# ma(4,5,6)
+# rezultat = 4+ ma(4,5,6)
+# print(rezultat)
+
+# def putere(a,b):
+#     c = a
+#     for i in range(1,b):
+#         c=c*a
+#     return c
+
+# # rezultat = putere(2,4) + putere(2,5) + putere(2,6)
+# # print(rezultat)
+# suma = 0
+# for i in range (1,11):
+#     suma = suma + putere(2,i)
+
+# print(suma)
+
+# def paritate(a):
+#     if a%2 == 0:
+#        print(a)
+
+# list1=[1,42,53,64,32]
+# for i in list1:
+#     paritate(i)
+
+
+# def paritate():
+#     a=int(input("introdu nr"))
+#     while a != 0:
+#         if a%2 == 0:
+#             print("par")
+#         else: 
+#             print("ïmpar")
+#         a=int(input("introdu nr"))
+#     else: print("äi introdus tasta 0")
+   
+# paritate()
+
+
+# def paritate(a):
+#     if a%2 == 0:
+#         print("par")
+#     else:
+#         print("impar")
+#     return a
+# a=1
+# while a != 0:
+#     a = input()
+#     paritate(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
