d7510e7b82dcf67359177bbab746052cbf174c7c
gabinhio91 <114810717+gabinhio91@users.noreply.github.com>
2023-03-21 20:07:19 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,101 @@
+# tuplul_meu= ("1", 2, "ardei iute")
+# print(type(tuplul_meu))
+# print(tuplul_meu)
+
+# #la tuple nu poti edita elementele
+# #nu poti adauga elemente 
+# #nu poti sterge elemente
+
+# print(tuplul_meu[1]) 
+
+# tuplu2 = (5)
+# tuplu3 = (5,)
+
+# print(type(tuplu2))
+# print(type(tuplu3))
+
+# tuplu = ("unu", "doi", "trei", "patru")
+# print(tuplu[2])
+# print(tuplu[-1])
+
+# # (var1, var2) = tuplu
+# var1 = tuplu[0]
+# var2 = tuplu[1]
+# print()
+# print(var1)
+# print(var2)
+
+# tuplu = ("unu", "doi", "trei", "patru")
+# tuplu2 = (1,2,3)
+
+# print(tuplu + tuplu2)
+# print ()
+# tuplu_r = (tuplu+ tuplu2)*2
+# tuplu_r += tuplu_r
+# print (tuplu_r)
+
+# tuplu = ("unu", 2, "trei", 2)
+# print(tuplu[-3:])
+# print(tuplu[2:])
+# print(tuplu[1:3])
+# print(tuplu[:3])
+
+# print("trei" in tuplu)
+# print("treijdami" in tuplu)
+
+# print(tuplu.count(2))
+# print(tuplu.index("trei"))
+
+# print(len(tuplu))
+# print(max(tuplu))
+# print(min(tuplu))
+
+# tuplu = ("unu", '2', "trei", "2")
+# tuplu_r = tuple(sorted(tuplu))
+# print(tuplu)
+# print(tuplu_r)
+
+# x = (1, 2 ,3)
+# y= list(x)
+# y[1] = 5
+# x= tuple(y)
+# print(x)
+# print(type(x))
+
+# dictionar = {"nume": "gigel",
+#              "prenume":"craciun",
+#              "varsta": 30}
+
+
+# print(dictionar)
+# print(dictionar["varsta"])
+
+# dictionar["nume"] = "mos"
+# print(dictionar)
+# print(type(dictionar))
+
+# print(len(dictionar))
+
+un_set = {'unu', 'doi', 'trei'}
+print(un_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
