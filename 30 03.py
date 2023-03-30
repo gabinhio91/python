839e0920d01a1bbc154fb75c4b06a938f02fb294
gabinhio91 <114810717+gabinhio91@users.noreply.github.com>
2023-03-30 20:00:39 +0300

    Add files via upload

---
+++ 30 03.py
@@ -0,0 +1,93 @@
+
+# fisier = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\fisierul_meu.txt", "w")
+
+# "r" - read
+# "w" - write
+# "a" - append
+# "x" - new_file
+
+# print(fisier.read(8))
+# print(fisier.read(5))
+# print()
+# print("acum de la capat")
+# print()
+# fisier.seek(0)
+# print(fisier.read(5))
+
+# fisier.readline()
+# print(fisier.readline())
+
+# print(fisier.readlines())
+
+# fisier = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\fisierul_meu2.txt", "w")
+
+# for linie in fisier.readlines():
+#     if linie.startswith("Ace"):
+#         print(linie)
+
+# fisier.write("Am adaugat ceva")
+# fisier.write("Am adaugat altceva acum")
+# fisier.close
+
+# fisier = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\fisierul_meu2.txt", "r")
+# print(fisier.read(50))
+
+# fisier = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\fisierul_exp.txt", "w")
+
+# fisier.write("hello world 7777")
+# fisier.close
+# fisier = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\fisierul_exp.txt", "r")
+# print(fisier.read())
+
+# fisier = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\fisierul_meu2.txt", "a")
+# fisier.write("ana are 2 mere")
+# fisier.close
+# fisier = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\fisierul_meu2.txt", "r")
+# print(fisier.read())
+
+
+# f = open("C:\\Users\\CSC Craiova 10\\Desktop\\python\\altfisier3.txt" , "x")
+# f=open("altfisier3.py", "w")
+# print(f.read())
+
+# import os 
+# os.remove("C:\\Users\\CSC Craiova 10\\Desktop\\python\\altfisier.txt")
+# if os.path.exists("altfisier.py"):
+#     os.remove("altfisier.py")
+# else:
+#     print("nu avem")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
